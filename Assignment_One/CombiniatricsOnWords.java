a3c5e10ae1dfd994a6005e5375434d5c7d985912
ejdhindsa <ejdhindsa@Gmail.com>
2024-09-30 16:18:50 -0500

    make initial chnages

--- Assignment_One/CombiniatricsOnWords.java
+++ Assignment_One/CombiniatricsOnWords.java
@@ -1,7 +1,32 @@
-package "Assignment_One"
+/*********************************************************
+ * ACS-2906-001 Assignment One
+ * 
+ * Name: Ekamjot Singh
+ * Student ID: 3167888
+ * 
+ * Description: The following assignment takes an interge value from the user and then divides into
+ * two separate integers based solely on the binary (bit) values of the original integer.
+ *   
+ * @author ejdhindsa
+ * GitHub link: https://github.com/ejdhindsa/ACS-2906_Assignments/tree/main/Assignment_One
+ * 
+ ********************************************************/
+
+//  import statements
+import java.util.Scanner;
 
 public class CombiniatricsOnWords {
     public static void main(String[] args) {
+
+        // creating a scanner object
+        Scanner kb = new Scanner(System.in);
+
+        // asking user for the int at which required operations will be performed
+        System.out.println("Please enter an integer value:");
+        int userInput = kb.nextInt();
+
+        // calling a method that will convert the integer into a binary value
+        int binaryValue = convertToBinary(userInput);
 
     } // end of main
 

af75b5740430f19b37b56b4fa1f7235c0658af9a
ejdhindsa <ejdhindsa@Gmail.com>
2024-10-06 14:11:59 -0500

    Make utility methods

--- Assignment_One/CombiniatricsOnWords.java
+++ Assignment_One/CombiniatricsOnWords.java
@@ -42,30 +42,23 @@ public class CombiniatricsOnWords {
         int[] binarySplitOne = new int[binaryBitSize];
         int[] binarySplitTwo = new int[binaryBitSize];
 
-        // since we need to split values from the least significant bit, we will reverse the binaryValue
-        ArrayList<Integer> reversedBinaryValue = binaryValue;
-        Collections.reverse(reversedBinaryValue);
-
+        // creating a flag outside for loop to alternate between adding in splitOne and splitTwo
         boolean addToSplitOne = true;
 
         // now creating a for loop that will iterate through reversedBinaryValue
-        for(int i = 0; i < reversedBinaryValue.size(); i++)
-        {
+        for (int i = 0; i < binaryValue.size(); i++) {
             // since arrays are already filled with zeroes, it skips an additional step of
             // filling with zeros
-            if(reversedBinaryValue.get(i) == 1)
-            {
+            if (binaryValue.get(i) == 1) {
                 // now working with the flag that the bit has been added to splitOne or not
-                if(addToSplitOne)
-                {
-                    binarySplitOne[i] = reversedBinaryValue.get(i);
+                if (addToSplitOne) {
+                    binarySplitOne[i] = binaryValue.get(i);
                     // now set the split to true so the flag is interchanged, and we can add value in split two
                     addToSplitOne = false;
 
                 } // end of if
-                else
-                {
-                    binarySplitTwo[i] = reversedBinaryValue.get(i);
+                else {
+                    binarySplitTwo[i] = binaryValue.get(i);
 
                     // now set the split to false so that we can add value to split one
                     addToSplitOne = true;
@@ -74,6 +67,10 @@ public class CombiniatricsOnWords {
             } // end of main if
 
         } // end of for
+
+        // now reversing the arrays to achieve the correct answer
+        reverseArray(binarySplitOne);
+        reverseArray(binarySplitTwo);
 
         // printing both arrays
         System.out.println();
@@ -89,9 +86,8 @@ public class CombiniatricsOnWords {
      *
      * @description The following method converts integer that has been passed a parameter to the method
      * into a binary value, which is then returned to the main method
-    *****************************************************/
-    public static ArrayList<Integer> convertToBinary(int userInput)
-    {
+     *****************************************************/
+    public static ArrayList<Integer> convertToBinary(int userInput) {
         // creating an arraylist that will hold the binary bits of the userInput
         ArrayList<Integer> binaryValues = new ArrayList<>();
 
@@ -101,8 +97,7 @@ public class CombiniatricsOnWords {
         int remainder = 0;
 
         // creating a while loop until the quotient becomes 0
-        while(quotient != 0)
-        {
+        while (quotient != 0) {
             // since we are working with decimal values, the remainder when divided by two
             // provides the binary conversion (when reversed)
             remainder = quotient % 2;
@@ -125,6 +120,10 @@ public class CombiniatricsOnWords {
 
     } // end of convert binary
 
+    // -------------------------------------------------------------
+    // -------------------- UTILITY METHODS ------------------------
+    // -------------------------------------------------------------
+
     /**************************************************************
      * @method printArrayList
      * @param arrayList Array List that is needed to be printed
@@ -134,11 +133,40 @@ public class CombiniatricsOnWords {
     public static void printArrayList(ArrayList<Integer> arrayList)
     {
         // using a for each loop to print the contents of arraylist
-        for ( int items : arrayList)
+        for (int items : arrayList)
         {
             System.out.print(items);
         } // end of for-each
 
     } // end of printArrayList
 
-} // end of class
+    /**************************************************************
+     * @method printArray
+     * @param array Array that is needed to be printed
+     *
+     * @description The following method prints the array to the console
+     *************************************************************/
+    public static void printArray(int[] array)
+    {
+        // printing the array using for loop
+        for (int items : array)
+        {
+            System.out.println(items);
+        } // end of for-each
+
+    } // end of printArray
+
+    /**************************************************************
+     * @method reverse
+     * @param array Array that needs to be reversed
+     *
+     * @description The following method reverses the array
+     *************************************************************/
+    static void reverseArray(int[] array)
+    {
+        // calling the Collections package and reversing the arrays
+        Collections.reverse(Arrays.asList(array));
+
+    } // end of class
+
+}
